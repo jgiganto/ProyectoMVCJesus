0b14a47f7508daeec7f8dcc3a7e7c2b972c4e527
jgiganto <jgiganto1@hotmail.com>
2018-02-01 14:16:26 +0100

    a

---
+++ ProyectoMVCJesus/Controllers/EmpleadosController.cs
@@ -0,0 +1,91 @@
+﻿using ProyectoMVCJesus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoMVCJesus.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        ModeloHospital modelo;
+
+        public EmpleadosController()
+        {
+            this.modelo = new ModeloHospital();
+        }
+
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TodosEmpleados(String orden,String direccion,String campo)
+        {
+            List<EMP> lista = modelo.GetEmpleados();
+            ViewBag.direccion = "ASC";
+            if (orden != null )
+            {
+                if (campo == "apellido")
+                {
+                    if (direccion == "ASC")
+                    {
+                        lista = lista.OrderBy(z => z.APELLIDO).ToList();
+                        ViewBag.direccion = "DESC";
+                    }
+                    else
+                    {
+                        lista = lista.OrderByDescending(z => z.APELLIDO).ToList();
+                    }
+                }
+
+                if (campo == "oficio")
+                {
+                    if (direccion == "ASC")
+                    {
+                        lista = lista.OrderBy(z => z.OFICIO).ToList();
+                        ViewBag.direccion = "DESC";
+                    }
+                    else
+                    {
+                        lista = lista.OrderByDescending(z => z.OFICIO).ToList();
+                    }
+                }
+
+                if (campo == "salario")
+                {
+                    if (direccion == "ASC")
+                    {
+                        lista = lista.OrderBy(z => z.SALARIO).ToList();
+                        ViewBag.direccion = "DESC";
+                    }
+                    else
+                    {
+                        lista = lista.OrderByDescending(z => z.SALARIO).ToList();
+                    }
+                }
+
+            }
+            return View(lista);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+//MIO
+//    lista = lista.OrderBy(z => z.APELLIDO).ToList();
+//    ViewBag.direccion = "DESC";
+
+//}
+//else
+//{
+//    lista = lista.OrderByDescending(z => z.APELLIDO).ToList();
+//}
---
+++ ProyectoMVCJesus/Models/ModeloHospital.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoMVCJesus.Models
+{
+    public class ModeloHospital
+    {
+        ContextoHospital contexto;
+
+        public ModeloHospital()
+        {
+            this.contexto = new ContextoHospital();
+        }
+
+        public List<EMP> GetEmpleados()
+        {
+            var consulta = from datos in contexto.EMP
+                           select datos;
+            return consulta.ToList();
+        }
+    }
+}
